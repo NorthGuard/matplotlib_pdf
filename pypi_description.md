396b5feaec524b2239f2e0ea6a35a18b6c84e7fe
Northguard <northguard_serve@tutanota.com>
2020-08-07 14:29:11 +0200

    PyPI description in setup.py.

--- pypi_description.md
+++
@@ -1,5 +0,0 @@
-Store multiple `matplotlib` figures as pages in a PDF-file.  
-`PDFFigureContainer` is initialized with a Path to a destination.  
-`PDFFigureContainer.add_figure_page()` is called to add current figure to PDF.
-
-More information at https://github.com/NorthGuard/matplotlib_pdf
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 from matplotlib_pdf import __version__
 
-with open("pypi_description.md", "r") as fh:
-    long_description = fh.read()
+long_description = """
+Store multiple `matplotlib` figures as pages in a PDF-file.  
+`PDFFigureContainer` is initialized with a Path to a destination.  
+`PDFFigureContainer.add_figure_page()` is called to add current figure to PDF.
+
+More information at https://github.com/NorthGuard/matplotlib_pdf
+"""
 
 setup(
     # Main
